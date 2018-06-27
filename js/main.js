47ff8c6fd4570e3d3c266186d28f91d6bb9b5202
Olivier Balaguer <oli@olivierbalaguer.com>
2018-06-28 00:24:49 +0100

    more typos. jquery body fade in.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 $(function() {
+  $('body').delay(1200).css('opacity', 1);
   $('.contact').replaceWith(`<a style="color: rgba(255, 255, 255, 0.7)" href="mailto:info@kiwiandroo.com?subject=Love your food">info@kiwiandroo.com</a>`)
 });
