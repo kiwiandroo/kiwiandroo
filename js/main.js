8321fef432e70a4c33b3af2528bac92d6922203b
Olivier Balaguer <oli@olivierbalaguer.com>
2018-06-28 00:04:49 +0100

    fixed typo. Changing vanilla JS with jquery for IE fix.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
-let el = document.querySelector('.contact');
-el.innerHTML = `<a href="mailto:info@kiwiandroo.com?subject=Love your food">info@kiwiandroo.com</a>`;
+$(function() {
+  $('.contact').replaceWith(`<a style="color: rgba(255, 255, 255, 0.7)" href="mailto:info@kiwiandroo.com?subject=Love your food">info@kiwiandroo.com</a>`)
+});
